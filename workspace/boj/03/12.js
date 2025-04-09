b60c4072fa14e89c995c66a45c7edba389932004
twkimfe <arthurkim135@naver.com>
2025-04-09 23:15:46 +0900

    feat: boj 03-11~12 04-01 문제 풀이

--- workspace/boj/03/12.js
+++ workspace/boj/03/12.js
@@ -28,3 +28,42 @@
 7
 */
 
+/**
+ * 표준 입력장치(콘솔)에서 n 줄로 입력된 줄당 n 건의 데이터를 읽어서 숫자로 변환한 후 배열로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열
+ */
+function getData() {
+  const fs = require("fs");
+  // '23 48\n25\n'
+  const fileData = fs.readFileSync(0).toString();
+  // ['23 48', '25']
+  const arr = fileData.trim().split("\n");
+
+  const result = []; // 2차원 배열 리턴
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i]; // '23 48', '25'
+    const rowArr = row.split(" "); // ['23', '48'], ['25']
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+
+    result.push(rowArr);
+  }
+  return result;
+}
+
+let result = "";
+function main() {
+  const data = getData();
+  //data에서 값을 꺼내서 문제 해결하는 코드 작성
+
+  for (let i = 0; i < data.length; i++) {
+    const a = data[i][0];
+    const b = data[i][1];
+
+    sum = a + b;
+    result += sum + "\n";
+  }
+}
+main();
+console.log(result.trim());
--- workspace/boj/04/01.js
+++ workspace/boj/04/01.js
@@ -39,3 +39,43 @@
 1
 */
 
+/**
+ * 표준 입력장치(콘솔)에서 n 줄로 입력된 줄당 n 건의 데이터를 읽어서 숫자로 변환한 후 배열로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열
+ */
+function getData() {
+  const fs = require("fs");
+  // '23 48\n25\n'
+  const fileData = fs.readFileSync(0).toString();
+  // ['23 48', '25']
+  const arr = fileData.trim().split("\n");
+
+  const result = []; // 2차원 배열 리턴
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i]; // '23 48', '25'
+    const rowArr = row.split(" "); // ['23', '48'], ['25']
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+
+    result.push(rowArr);
+  }
+  return result;
+}
+
+let result = "";
+function main() {
+  const data = getData();
+  //data에서 값을 꺼내서 문제 해결하는 코드 작성
+  const list = data[1];
+  const target = data[2][0];
+  let count = 0;
+  for (let num of list) {
+    if (target === num) {
+      count++;
+    }
+  }
+  console.log(count);
+}
+main();
+console.log(result.trim());
