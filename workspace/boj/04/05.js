21e7b65e36a36cd50ca406cd00eefb4b7dbb4dec
twkimfe <arthurkim135@naver.com>
2025-04-11 10:03:18 +0900

    feat: boj 04-05 문제 풀이

--- workspace/boj/04/05.js
+++ workspace/boj/04/05.js
@@ -34,5 +34,59 @@
 2 2 2
 예제 출력 1 
 1 2 1 1 0
+
+예제 입력 2 
+4 3
+1 2 2
+2 3 4
+2 2 1
+
+예제 출력 2
+2 1 4 0
 */
 
+/**
+ * 표준 입력장치(콘솔)에서 여러줄로 입력된 줄당 여러 건의 데이터를 읽어서 숫자로 변환한 후
+ * 배열로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열
+ */
+function getData() {
+  const fs = require("fs");
+  return fs
+    .readFileSync(0)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((row) =>
+      row.split(" ").map((val) => (isNaN(val) ? val : parseInt(val)))
+    );
+}
+
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  // console.log(data);
+  const n = data[0][0];
+  const result = new Array(n).fill(0);
+
+  for (let i = 1; i < data.length; i++) {
+    const rowArr = data[i];
+    const x = rowArr[0];
+    const y = rowArr[1];
+    const z = rowArr[2];
+
+    for (let k = x; k <= y; k++) {
+      result[k - 1] = z;
+    }
+  }
+  const strResult = result.join(" ");
+  console.log(strResult);
+
+  /* 문자열을 다시 배열로 변환하는 코드
+    const reArray = strResult
+    .split(" ")
+    .map((val) => (isNaN(val) ? val : parseInt(val)));
+  console.log(reArray);
+  */
+}
+main();
