f3b19e3f7781f968586589db5a972c61e2d1ace7
twkimfe <arthurkim135@naver.com>
2025-04-29 18:53:54 +0900

    feat: ex06-27 코드 학습

--- workspace/ch06/ex06-17-02.ts
+++ workspace/ch06/ex06-17-02.ts
@@ -1,6 +1,18 @@
 // echo 함수 - 제네릭 함수
 // ex06-17.ts 복사
 
-(()=>{
-  
+(() => {
+  // T가 아닌 아무 문자를 사용할 수 있지만, 관례적으로 Type의 첫 글자인 T를 사용
+  function echo<T>(msg: T): T {
+    return msg;
+  }
+
+  // 타입 별칭도 추가하여 구현 가능
+  type name = string | number;
+  function echo2<name>(msg: name): name {
+    return msg;
+  }
+
+  console.log(echo<string>("hello"));
+  console.log(echo2<name>(2));
 })();
--- workspace/ch06/ex06-17-03.ts
+++ workspace/ch06/ex06-17-03.ts
@@ -1,6 +1,29 @@
 // echo 함수 - 제네릭 타입 제약
 // ex06-17-02.ts 복사
 
-(()=>{
-  
+(() => {
+  // T가 아닌 아무 문자를 사용할 수 있지만, 관례적으로 Type의 첫 글자인 T를 사용
+  function echo<T extends string | number>(msg: T): T {
+    return msg;
+  }
+
+  console.log(echo<string>("hello"));
+  console.log(echo<number>(100));
+  // console.log(echo<boolean>(true)); // type 제약을 해서 타입 에러 발생
+
+  const str = echo<string>("hello");
+  const num = echo<number>(123.456);
+
+  console.log(str.toUpperCase(), num.toFixed(2));
+
+  function echo2<T extends { length: number }>(msg: T): T {
+    return msg;
+  }
+
+  // echo2<number>(100); // type 에러 발생
+
+  const str2 = echo2<string>("hello"); // 문자열의 length는 5다.
+  const num2 = echo2<number[]>([10, 20]);
+
+  console.log(str2.length, num2.length);
 })();
