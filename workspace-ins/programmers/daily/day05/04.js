2c15520c030d8d394928c7e4230e6ede23df9204
Kilyong Jeong <uzoolove@gmail.com>
2025-05-30 17:21:25 +0900

    ✨ 5일차 완료

--- workspace-ins/programmers/daily/day05/04.js
+++ workspace-ins/programmers/daily/day05/04.js
@@ -26,6 +26,17 @@ num_list	result
 
 function solution(num_list) {
   var answer = 0;
+
+  let sum = 0;
+  let mul = 1;
+
+  // [3, 4, 5, 2, 1]
+  for(let i=0; i<num_list.length; i++){
+    sum += num_list[i]; // 0 + 3 + 4 + 5 + 2 + 1
+    mul *= num_list[i]; // 1 * 3 * 4 * 5 * 2 * 1
+  }
+  answer = mul < sum**2 ? 1 : 0;
+
   return answer;
 }
 
--- workspace-ins/programmers/daily/day05/05.js
+++ workspace-ins/programmers/daily/day05/05.js
@@ -30,6 +30,19 @@ num_list	result
 
 function solution(num_list) {
   var answer = 0;
+
+  let odd = '';
+  let even = '';
+
+  for(let i=0; i<num_list.length; i++){
+    if(num_list[i] % 2 === 0){
+      even += num_list[i];
+    }else{
+      odd += num_list[i];
+    }
+    answer = parseInt(odd) + parseInt(even);
+  }
+
   return answer;
 }
 
